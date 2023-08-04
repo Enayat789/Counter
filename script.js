f1de01a38c725f685d0f52eac829ee871462597f
enayat789 <enayatbhai2006@gmail.com>
2023-08-04 22:55:03 +0530

    script.js file created

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+let data = 0;
+ document.getElementById("counter_value").innerText = data;
+ 
+//creation of increment function
+function increament() {
+    data += 1;
+    document.getElementById("counter_value").innerText = data;
+}
+//creation of decrement function
+function decreament() {
+    data -= 1;
+    document.getElementById("counter_value").innerText = data;
+}
 
+function reset(){
+document.getElementById("counter_value").value = "";
+}
