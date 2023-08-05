5510a3a6aefdb06734d5157931dd04e629b9dcb1
enayat789 <enayatbhai2006@gmail.com>
2023-08-05 12:00:08 +0530

    set the color of odd & even no.

--- script.js
+++ script.js
@@ -1,17 +1,44 @@
 let data = 0;
- document.getElementById("counter_value").innerText = data;
- 
+document.getElementById("counter_value").innerText = data;
+
 //creation of increment function
 function increament() {
-    data += 1;
-    document.getElementById("counter_value").innerText = data;
+  data += 1;
+  document.getElementById("counter_value").innerText = data;
+  document.getElementById("decrease_btn").innerHTML = "DECREASE";
+
+  if (data % 2 == 0) {
+    document.getElementById("counter_value").style.color = "red";
+  } else {
+    document.getElementById("counter_value").style.color = "blue";
+  }
 }
 //creation of decrement function
 function decreament() {
+  if (data >= 1) {
     data -= 1;
     document.getElementById("counter_value").innerText = data;
+  } else {
+    counter_valuexddtdr;
+    document.getElementById("decrease_btn").innerHTML = "Btn DISABLED";
+  }
+
+  if (data % 2 == 0) {
+    document.getElementById("counter_value").style.color = "red";
+  } else {
+    document.getElementById("counter_value").style.color = "blue";
+  }
+
+  if (data === 0) {
+    document.getElementById("counter_value").style.color = "deeppink";
+  }
 }
 
-function reset(){
-document.getElementById("counter_value").innerText = '';
+function reset() {
+  console.log(data);
+  data = 0;
+  document.getElementById("counter_value").innerHTML = data;
+  if (data === 0) {
+    document.getElementById("counter_value").style.color = "deeppink";
+  }
 }
